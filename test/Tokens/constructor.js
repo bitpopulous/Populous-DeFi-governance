8f1dd9ef1d2190e2a64839e3cb647118ad74b12b
najienka <nemitariajienka@gmail.com>
2021-04-23 12:08:18 +0100

    constructor tests for tokens

---
+++ test/Tokens/constructor.js
@@ -0,0 +1,21 @@
+const {
+    deployMockPPT, 
+    deployMockPXT, 
+    deployMockVotingToken
+} = require('../helpers/deploy');
+
+describe('MockPPT deployment', accounts => {
+    xit('should deploy with valid contract address');
+    xit('should deploy with valid metadata - name, symbol, decimals');
+});
+
+describe('MockPXT deployment', accounts => {
+    xit('should deploy with valid contract address');
+    xit('should deploy with valid metadata - name, symbol, decimals');
+});
+
+describe('MockVotingToken deployment', accounts => {
+    xit('should deploy with valid contract address');
+    xit('should deploy with valid metadata - name, symbol, decimals');
+    xit('should deploy with correct owner/admin');
+});
