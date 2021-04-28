5266206f19f2051fbbe604f90cbf1d6536082b9b
najienka <nemitariajienka@gmail.com>
2021-04-28 10:58:16 +0100

    failed proposal test passed

--- test/Governance/states.js
+++ test/Governance/states.js
@@ -62,8 +62,6 @@ describe('Proposal States', () => {
 
     xit('canceled proposal');
 
-    xit('failed proposal');
-
 
     it('should move to active state', async () => {
         await advanceBlock();
@@ -217,5 +215,146 @@ describe('Proposal States', () => {
 
     xit('expired proposal');
     
+
+})
+
+
+
+describe('Failed Proposal', () => {
+    
+    let 
+    governanceInstance,
+    votingTokenInstance,
+    pptInstance,
+    pxtInstance,
+    governanceStrategyInstance,
+    votingDelay,
+    guardian,
+    executorInstance,
+    proposalTx,
+    proposalId;
+
+    before(async () => {
+        [
+            governanceInstance,
+            votingTokenInstance,
+            pptInstance,
+            pxtInstance,
+            governanceStrategyInstance,
+            votingDelay,
+            guardian,
+            executorInstance,
+            {
+                owner, 
+                firstUser, 
+                secondUser, 
+                thirdUser, 
+                fourthUser
+            }
+        ] = await deployExecutor();
+
+        const encodedArgument = ethers.utils.defaultAbiCoder.encode(['address'], [firstUser]);
+        
+        proposalTx = await governanceInstance.create(
+            executorInstance.address, [pptInstance.address], ['0'], 
+            ['balanceOf(address)'], [encodedArgument], [false], 
+            ipfsBytes32Hash, { from: firstUser });
+        
+        proposalId = proposalTx.logs[0].args.id;
+    });
+
+    it('should move to pending state', async () => {
+        expectBignumberEqual(await governanceInstance.getProposalState(proposalId), PROPOSAL_STATES.Pending);
+    });
+
+    it('should move to active state', async () => {
+        await advanceBlock();
+        // no voting delay
+        expectBignumberEqual(await governanceInstance.getProposalState(proposalId), PROPOSAL_STATES.Active);
+    });
+
+    it('should estimate correct voting outcome for user based on token (PPT) amount', async () => {
+        expectBignumberEqual(await governanceInstance.getProposalState(proposalId), PROPOSAL_STATES.Active);
+
+        const mintAmount = 1000000 * (10 ** 8);
+        const {owner, firstUser, secondUser, thirdUser, fourthUser} = await getGovernanceActorsAsync();
+        for (let user of [firstUser, secondUser, thirdUser, fourthUser]) {
+            expectBignumberEqual(await pptInstance.balanceOf(user), mintAmount);
+            expectBignumberEqual(await pxtInstance.balanceOf(user), mintAmount);
+        }
+
+        const outcome = await governanceInstance.getVotingOutcome(
+            pptInstance.address, mintAmount/2,
+            proposalId, true, {from: thirdUser});
+        
+        expect(outcome.forVotes, mintAmount); // ppt issued x2 of voting token
+        expect(outcome.againstVotes, 0);
+    });
+
+
+    it('should estimate correct voting outcome for user based on token (PXT) amount', async () => {
+        expectBignumberEqual(await governanceInstance.getProposalState(proposalId), PROPOSAL_STATES.Active);
+
+        const mintAmount = 1000000 * (10 ** 8);
+        const {owner, firstUser, secondUser, thirdUser, fourthUser} = await getGovernanceActorsAsync();
+        for (let user of [firstUser, secondUser, thirdUser, fourthUser]) {
+            expectBignumberEqual(await pptInstance.balanceOf(user), mintAmount);
+            expectBignumberEqual(await pxtInstance.balanceOf(user), mintAmount);
+        }
+
+        const outcome = await governanceInstance.getVotingOutcome(
+            pxtInstance.address, mintAmount/2,
+            proposalId, false, {from: secondUser});
+        
+        expect(outcome.forVotes, 0); // ppt issued x2 of voting token
+        expect(outcome.againstVotes, mintAmount/2);
+    });
+
+    it('should vote and move to failed state', async () => {
+        expectBignumberEqual(await governanceInstance.getProposalState(proposalId), PROPOSAL_STATES.Active);
+        
+        const mintAmount = 1000000 * (10 ** 8);
+
+        // voting duration is 5 blocks, 5 mined TXs on ganache 
+        // (one await advanceBlock(); in active test above) to get proposal from pending to active
+
+        // 3 votes for with different tokens        
+        await governanceInstance.submitVote(
+            pxtInstance.address, mintAmount/2,
+            proposalId, false, {from: firstUser}
+        )
+
+        await governanceInstance.submitVote(
+            pptInstance.address, mintAmount/2,
+            proposalId, false, {from: secondUser}
+        )
+
+        await governanceInstance.submitVote(
+            pxtInstance.address, mintAmount/2,
+            proposalId, false, {from: thirdUser}
+        )
+
+        // 1 vote against
+        await governanceInstance.submitVote(
+            pxtInstance.address, mintAmount/2,
+            proposalId, true, {from: fourthUser}
+        )
+
+        const proposal = await governanceInstance.getProposalById(proposalId);
+
+        // check locked PXT/PPT tokens and amount
+        const fourthUserLockedTokens = await governanceInstance.getLockedTokens(proposalId, fourthUser);
+        // check vote struct for voter support and voting power
+        const fourthUserVotes = await governanceInstance.getVoteOnProposal(proposalId, fourthUser);
+        
+        expectBignumberEqual(fourthUserLockedTokens.amount, mintAmount/2);
+        expect(fourthUserLockedTokens.tokenAddress).to.be.equal(pxtInstance.address);
+        expectBignumberEqual(fourthUserVotes.votingPower, mintAmount/2);
+        expect(fourthUserVotes.support).to.be.equal(true);
+
+        await advanceBlock();
+
+        expectBignumberEqual(await governanceInstance.getProposalState(proposalId), PROPOSAL_STATES.Failed);
+    });
 
 })
