a9745536c77a0d77578777a5d2488802191fa251
najienka <nemitariajienka@gmail.com>
2021-04-28 11:06:03 +0100

    removed unused files

--- test/Governance/voting.js
+++ test/Governance/voting.js
@@ -1,9 +1,4 @@
 
-//voting
-xit('approve tokens to swap before voting');
-xit('vote should fail, if tokens to swap are not approved');
-xit('should lock correct amount of tokens after voting');
-xit('should get correct amount of lock tokens after voting');
-xit('should redeem correct amount of lock tokens after voting');
-xit('should not redeem correct amount of lock tokens after voting if voting tokens are transferred');
-xit('should get correct votes and bool support for voter');
+//voting - negative cases
+xit('vote should fail, if tokens to swap are not approved before voting');
+xit('should not redeem valid/correct amount of lock tokens after voting if voting tokens are transferred');
